29afa63719f1d39a52ea5825de6e774d59bc78ef
Abby Christens <achristens@gmail.com>
2017-11-14 20:56:27 -0500

    adding log in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:name])
     if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect_to chats_url
-  else
-    render :new
+      session[:user_id] = user.id
+      redirect_to chats_url
+    else
+      render :new
+    end
   end
 
   def destroy
