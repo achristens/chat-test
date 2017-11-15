0e7f25c8e7a6f2276d3cbba2d7a3fcd0d106cf32
Abby Christens <achristens@gmail.com>
2017-11-14 20:50:12 -0500

    Creating Chats view and adding sessions create variables

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect_to chats_url
+  else
+    render :new
+  end
+
+  def destroy
+  end
 end
